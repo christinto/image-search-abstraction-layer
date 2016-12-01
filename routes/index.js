332e3b7ce49bc62d3cb3778b709bd47bf342c142
Jeff <jeff_m_england@yahoo.com>
2016-12-01 16:44:16 +0000

    bug fixing

--- routes/index.js
+++ routes/index.js
@@ -4,21 +4,31 @@ var imgur = require("../services/imgur");
 var path = require("path");
 var History = require("../models/history");
  
-router.get('/',function(req,res){
+ 
+router.get("/",function(req,res){
   res.sendFile(path.join(__dirname+'/index.html'));
 });
- 
-router.get("/latest", function(req, res) {
-    History.find({}, "term when -_id").sort("-when").limit(10).then(function (results) {
-        res.json(results);
+
+
+router.get("/latest", function (req, res) {
+  History.find({})
+    .select('term when -_id')
+    .sort('-when')
+    .limit(10)
+    .then(function (results) {
+      res.json(results);
     });
 });
- 
+
+
 router.get("/search/:q", function (req, res) {
-    
-    imgur.getImage(req.params.q, req.query.offset).then(function (ans) {
-        new History({ term: req.params.q }).save();
-        res.json(ans);
+  imgur.getImage(req.params.q, req.query.offset)
+    .then(function (ans) {
+      History.create({ term: req.params.q })
+        .then(function(history) {
+          console.log(history); // Save history successfully
+        });
+      res.json(ans);
     });
 });
  
